e67dc54fa662e29011bccfd1122b3a21410f4ed1
Ryley Hindman <cessnageek@gmail.com>
2021-02-21 17:57:29 -0600

    Added GPIO logic

--- octoprint_camera_light/__init__.py
+++ octoprint_camera_light/__init__.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
+import RPi.GPIO as GPIO
+GPIO.setup(18, GPIO.OUT)
 #import testCustom
 
 class camera_lightPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.SimpleApiPlugin, octoprint.plugin.TemplatePlugin):
@@ -10,9 +12,13 @@ class camera_lightPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.Simple
 
 	def on_api_get(self, request):
 		print("In api get!!!!!!_--------------")
-		f = open("testfile.txt", "a")
-		f.write("clicked button\n")
-		f.close()
+		#f = open("testfile.txt", "a")
+		#f.write("clicked button\n")
+		#f.close()
+		if GPIO.input(18):
+			GPIO.output(18, GPIO.LOW)
+		else:
+			GPIO.output(18, GPIO.HIGH)
 		return "Success"
 
 __plugin_pythoncompat__ = ">=2.7,<4"
